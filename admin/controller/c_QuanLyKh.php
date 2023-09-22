8f79154c07f5d11c549aeb555496f417822d3702
Huyết tử Cool <baokyeu12345@gmail.com>
2023-09-22 19:59:39 +0700

    29/9

--- admin/controller/c_QuanLyKh.php
+++ admin/controller/c_QuanLyKh.php
@@ -1,4 +1,5 @@
 <?php
+if(isset($_SESSION['ss_admin'])){
     //Kiểm tra xem có tồn tại keyword trên thanh url không
     if (isset($_GET['keyword'])) {
         $keyword = $_GET['keyword'];
@@ -8,5 +9,8 @@
         //Nếu không tồn tại thì sẽ lấy tất cả dữ liệu trong bảng product
         $data_khachhang=$db->get('khach_hang', array());
     }
+}else{
+    header('location: ?page=login');
+}
     include "./view/v_QuanLyKh.php";
 ?>
